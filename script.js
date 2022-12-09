c45e6003974c2b361aecf5f224a1bef9eb3255d9
Ashish Chaudhary <ashish080303@gmail.com>
2022-12-09 09:54:20 +0530

    main logic applied

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-const brth = document.querySelector('#birthday');
-const lucky = document.querySelector('#luckyNo');
+let birth = document.querySelector('#birthday');
+let lucky = document.querySelector('#luckyNo');
+const btn = document.querySelector('#find');
+const display = document.querySelector('#display'); 
+
+btn.addEventListener('click',()=>{
+  birth = birth.value;
+  lucky = lucky.value;
+  birth = birth.slice(0,4)+birth.slice(5,7)+birth.slice(8,10)
+  
+  if(birth%lucky==0){
+    displayMessage("Yay! Your birthday is lucky.")
+  } 
+  else {
+    displayMessage("Sorry, but your birthday is not lucky.");
+  }
+});
+function displayMessage(mssg){
+  display.innerText =  mssg; 
+}
